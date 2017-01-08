73157b9dfd37a2524d81241724cb274c26b3610f
Jental <jentalmeister@gmail.com>
2017-01-08 13:10:50 +0300

    build folder

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ module.exports = {
     './src/index.js'
   ],
   output: {
-    path: __dirname,
+    path: __dirname + '/build/',
     publicPath: '/',
     filename: 'bundle.js'
   },
@@ -21,6 +21,6 @@ module.exports = {
   },
   devServer: {
     historyApiFallback: true,
-    contentBase: './'
+    contentBase: './build/'
   }
 };
